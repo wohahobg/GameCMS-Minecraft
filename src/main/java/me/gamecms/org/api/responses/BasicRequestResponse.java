d73ccec916b318811081faedc7db44d5c5d513d2
BuildTools <unconfigured@null.spigotmc.org>
2023-06-13 09:57:24 +0100

    Class name changes ApiRequestResponseMain.java -> BasicRequestResponse.java

--- src/main/java/me/gamecms/org/api/responses/BasicRequestResponse.java
+++ src/main/java/me/gamecms/org/api/responses/BasicRequestResponse.java
@@ -1,9 +1,9 @@
-package me.gamecms.org.api;
+package me.gamecms.org.api.responses;
 
 
 import java.util.Map;
 
-public class ApiRequestResponseMain {
+public class BasicRequestResponse {
     public static String bad_request_format = "{\"status\":500, \"message\":\"Something went wrong\"}";
     public int status;
     public String message;
